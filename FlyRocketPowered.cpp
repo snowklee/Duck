13da6ffedec7b96d8b36d3ee1f637058eff31fed
Sasha <Sasha>
2021-09-07 15:06:06 +0700

    добавление FlyRocketPowered

---
+++ FlyRocketPowered.cpp
@@ -0,0 +1,6 @@
+#include "FlyRocketPowered.h"
+
+void FlyRocketPowered::fly()
+{
+	cout << "I'm flying with a Rocket!" << endl;
+}
---
+++ FlyRocketPowered.h
@@ -0,0 +1,12 @@
+#ifndef FLYROCETPOWERED_H
+#define FLYROCETPOWERED_H
+
+#include "FlyBehavior.h"
+
+class FlyRocketPowered : public FlyBehavior
+{
+  public:
+	  void fly();
+};
+
+#endif
