3c9a1f4065e5c46818fff755420a67b32c90b148
Sasha <Sasha>
2021-09-07 14:46:15 +0700

    добавление полета

---
+++ FlyBehavior.cpp
@@ -0,0 +1,6 @@
+#include "FlyBehavior.h"
+
+void FlyBehavior::fly()
+{
+
+}
---
+++ FlyBehavior.h
@@ -0,0 +1,12 @@
+
+#ifndef FLYBEHAVIOR_H
+#define FLYBEHAVIOR_H
+
+class FlyBehavior
+{
+  public: 
+	  virtual void fly() = 0;
+};
+
+#endif
+
