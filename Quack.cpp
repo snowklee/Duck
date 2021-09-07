3697af10a592e176a31dfc734ae34c00186b7e5c
Sasha <Sasha>
2021-09-07 14:57:55 +0700

    Кряканье

---
+++ Quack.cpp
@@ -0,0 +1,6 @@
+#include "Quack.h"
+
+void Quack::quack()
+{
+	cout << "Quack" << endl;
+}
---
+++ Quack.h
@@ -0,0 +1,13 @@
+#ifndef QUACK_H
+#define QUACK_H
+
+#include "QuackBehavior.h"
+
+class Quack : public QuackBehavior
+{
+public:
+	void quack();
+};
+
+
+#endif
